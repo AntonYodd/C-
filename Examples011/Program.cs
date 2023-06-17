1f6ebf2feefe9d2b7ebeff6c67cd44bff8162000
Anton Kholodov <antonnn333@yandex.ru>
2023-06-17 17:08:05 +0300

    011

---
+++ Examples011/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int i=1;
+while (i<=number)
+{
+    System.Console.Write(Math.Pow(i , 2) + " ");
+    i++;
+}
+
+
+//тоже самое только через For
+
+//System.Console.Write("Введите число: ");                    
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//for(int i = 1; i <= number; i++)
+//{
+  //  Console.Write(Math.Pow(i, 2) + " ");
+//}
