017d50fc50384eb499ed5a7b5e10e637e6dc8e68
Anton Kholodov <antonnn333@yandex.ru>
2023-07-08 15:32:30 +0300

    seminar 9

---
+++ Example035/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumNumbers(int X)
+{
+    if (X == 0) return 0;
+    return X % 10 + SumNumbers(X / 10);
+}
+
+
+
+int X = ReadInt("Введите число: ");
+System.Console.WriteLine($" Сумма цифр введённого числа равна: {SumNumbers(X)}");
---
+++ example032/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void Numbers(int N)
+{
+    if (N == 0) return;
+    Numbers(N - 1);
+    System.Console.Write(N + " ");
+
+}
+
+int N = ReadInt("Введите число");
+Numbers(N);
